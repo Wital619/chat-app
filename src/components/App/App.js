b56c0d4d5f195dd4a26c2a4095a9f3dd5ce5473f
Vitaliy Pogoretskyy <vitpogoretsky@gmail.com>
2018-11-28 19:14:15 +0200

    Add and configure all main files

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import styles from './App.css';
+
+class App extends Component {
+  render () {
+    return (
+      <div className={styles.app}>You see me !</div>
+    );
+  }
+}
+
+export default App;
---
+++ stylelint.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: [
+    "stylelint-config-standard"
+  ],
+  rules: {
+    indentation: 2,
+    "number-leading-zero": null,
+    "selector-pseudo-class-no-unknown": [true, {
+      ignorePseudoClasses: ["global"]
+    }],
+    "selector-max-id": 0,
+    "selector-class-pattern": "^([a-z][a-z0-9]*)(-[a-z0-9]+)*$"
+  }
+};
